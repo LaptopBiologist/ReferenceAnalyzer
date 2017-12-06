b1fc510b5113aa4605f39a993413a02608de5973
ThisIsMyUsername <mmcgurk87@gmail.com>
2017-12-06 12:11:34 -0500

    Added a new module which will have functions for annotating repeats based on UCSC TableBrowser tracks as well as extracting repeats from the reference based on known coordinates.

---
+++ AddAnnotations.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose: This contain functions for two purposes
+# 1) Annotate tandem repeats with the features they span
+# 2) Extract tandem repeats based on known coordinates
+#
+# Author:      I am
+#
+# Created:     06/12/2017
+# Copyright:   (c) I am 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import numpy
+import HTSeq
+import csv
+
+
+
+def
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
